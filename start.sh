a81d7d7e77c610131909d038ae07093da05b25dc
Maciej <maciej.wieczorek@fastwhitecat.com>
2017-06-27 10:06:36 +0200

    Init project

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$LISTEN_PORT" ]; then
+    LISTEN_PORT="80"
+fi
+
+if [ -z "$REDIRECT_URL" ]; then
+    REDIRECT_URL=""
+fi
+
+
+sed -i 's=__listen_port__='"$LISTEN_PORT"'=g' /etc/nginx/conf.d/default.conf
+sed -i 's=__redirect_url__='"$REDIRECT_URL"'=g' /etc/nginx/conf.d/default.conf
+
+nginx -g 'daemon off;'
